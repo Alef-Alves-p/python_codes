ba6609a2419bfcf9b9cb662c24cd9d5ee044a8db
alef-alves <alefpalves@gmail.com>
2024-04-13 15:36:24 -0300

    exercícios com laço while

--- curso_py_OtavioMiranda/rescunho2.py
+++ curso_py_OtavioMiranda/rescunho2.py
@@ -3,23 +3,24 @@
 frase = 'O Python é uma linguagem de programação'\
     'multiparadigma' \
     'Python foi criada po Guido Van Rossum'
-contagem_letra = {}
+contagem_letras = {}
 
 i = 0
 while i < len(frase):
     letra = frase[i]
     if letra.isalpha():
-        if letra in contagem_letra:
-            contagem_letra[letra] += 1
+        if letra in contagem_letras:
+            contagem_letras[letra] += 1
         else:
-            contagem_letra[letra] = 1
-
+            contagem_letras[letra] = 1
     i += 1
 
-if contagem_letra:  # Verifica se há letras na contagem
-    letra_mais_comum = max(contagem_letra, key=contagem_letra.get)
-    quantidade_mais_comum = contagem_letra[letra_mais_comum]
+if contagem_letras:  # Verifica se há letras na contagem
+    letra_mais_comum = max(contagem_letras, key=contagem_letras.get)
+    quantidade_mais_comum = contagem_letras[letra_mais_comum]
 
     print(f"A letra mais comum é '{letra_mais_comum}' com {quantidade_mais_comum} ocorrências.")
 else:
     print("A frase não contém letras.")
+
+"""
