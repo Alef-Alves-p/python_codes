afe270eb1b540607c85e1354a238f9a499b3f5c7
Alef-Alves-p <alefpalves@gmail.com>
2024-06-19 18:05:21 -0300

    atualizando exercícios

--- curso_py_OtavioMiranda/ex_questionario.py
+++ curso_py_OtavioMiranda/ex_questionario.py
@@ -1,35 +1,59 @@
 # crie um sistema de perguntas e respostas com python
 
-perguntas = [
+# Exercício - sistema de perguntas e respostas
 
+
+perguntas = [
     {
-        'Pergunta': 'Quanto é 4 * 5 ? ',
-        'Opções': ['16', '24', '35', '20'],
-        'Resposta': '20',
-    },
-    {
-        'Pergunta': 'Quanto é 12 / 3 ? ',
-        'Opções': ['6', '4', '3', '0'],
+        'Pergunta': 'Quanto é 2+2?',
+        'Opções': ['1', '3', '4', '5'],
         'Resposta': '4',
     },
     {
-        'Pergunta': 'Quanto é 40 - 15 ? ',
-        'Opções': ['25', '24', '15', '30'],
+        'Pergunta': 'Quanto é 5*5?',
+        'Opções': ['25', '55', '10', '51'],
         'Resposta': '25',
-
-
+    },
+    {
+        'Pergunta': 'Quanto é 10/2?',
+        'Opções': ['4', '5', '2', '1'],
+        'Resposta': '5',
     },
 ]
 
+qtd_acertos = 0
 for pergunta in perguntas:
     print('Pergunta:', pergunta['Pergunta'])
     print()
 
-    qtd_opcoes = pergunta['Opções']
-    for i, opcao in enumerate(pergunta['Opções']):
+    opcoes = pergunta['Opções']
+    for i, opcao in enumerate(opcoes):
         print(f'{i})', opcao)
-        print()
+    print()
 
     escolha = input('Escolha uma opção: ')
-    if escolha == enumerate(pergunta['Opções']):
-        print('Acertou!')
+
+    acertou = False
+    escolha_int = None
+    qtd_opcoes = len(opcoes)
+
+    if escolha.isdigit():
+        escolha_int = int(escolha)
+
+    if escolha_int is not None:
+        if escolha_int >= 0 and escolha_int < qtd_opcoes:
+            if opcoes[escolha_int] == pergunta['Resposta']:
+                acertou = True
+
+    print()
+    if acertou:
+        qtd_acertos += 1
+        print('Acertou 👍')
+    else:
+        print('Errou ❌')
+
+    print()
+
+
+print('Você acertou', qtd_acertos)
+print('de', len(perguntas), 'perguntas.')
